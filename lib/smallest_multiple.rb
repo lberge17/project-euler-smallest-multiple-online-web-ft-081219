f86e6a8baef2d9f52efc322a8322182134f3c4b9
Laura Berge <lauraeberge@gmail.com>
2020-06-30 10:23:15 -0500

    Done.

--- lib/smallest_multiple.rb
+++ lib/smallest_multiple.rb
@@ -1,1 +1,17 @@
-# Implement your procedural solution here!
+def smallest_multiple(limit)
+    multiple = 1
+    (2..limit).each do |divisor|
+        # the number multiplied by each number in the limit and then divided by the greatest common factor of both
+        multiple *= divisor / gcd(multiple, divisor)
+    end
+    multiple
+end
+  
+def gcd(multiple, divisor)
+    while divisor > 0
+        multiple %= divisor
+        return divisor if multiple == 0
+        divisor %= multiple
+    end
+    multiple
+end
